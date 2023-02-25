2305a413360c8ba4d3254cbc7735fa846dfef20e
unknown <juniomiguelsizeg@gmail.com>
2023-02-25 02:07:41 -0300

    'versão_1.0.1'

--- get_phrasals_mairo.py
+++ get_phrasals_mairo.py
@@ -55,21 +55,51 @@ def click_scroll():
 		pyg.click(1358,604,interval=0.25)
 	sleep(2)
 
+	#volta para a home da scrollbar
 	for n in range(5):
 		pyg.click(1354,102,interval=0.25)
 	sleep(10)
 	return click_scroll()
 
+def click_post():
+	#primeiro post: x=333, y=201
+	pyg.click(1358,284,interval=0.25)
+	sleep(0.25)
+	pyg.click(333,201)
+	select_text()
+	#segundo post: x=682, y=200
+	#terceiro post: x=327, y=486
+	#quarto post: x=673, y=491
+
+	#penúltimo post: x=333, y=201
+	#último post: x=682, y=200
+	return 1 
 
 def drag_bar():
 
 	pyg.mouseDown(button="left")
 
-def select_text(xi1,xf1,xi2,yf2):
-	pyg.click(xi1,yf1)
+def select_text():
+	#início do conteúdo do post: x=179, y=343
+	#aisle de término de conteúdo: x=832, y=526
+	sleep(10)
+	pyg.click(179,343, interval=0.25)
 	pyg.keyDown('shift')
-	pyg.click(123,345)
-	pyg.click(xi2,yf2)
+	for n in range(4):
+		pyg.click(1358,501,interval=0.25)
+	sleep(10)
+	pyg.click(832,526)
+	pyg.keyUp('shift')
+	#pyg.hotkey("ctrl","a")
+	sleep(0.25)
+	pyg.hotkey("ctrl","c")
+
+	content = clipboard.paste()
+	
+	with open('C:/Users/Miguel/Desktop/run_in.txt','wt', encoding='utf-8') as file:
+		file.write(content)
+	
+	pyg.alert("ARQUIVO CRIADO E SALVO !")
 
 def get_position():
 	#botão inferior da scrowbar: x=1357, y=716 ou um pouco fora x=1356, y=709
@@ -90,7 +120,7 @@ def get_position():
 
 if __name__ == '__main__':
 	#navigate('https://www.mairovergara.com/category/phrasal-verbs')
-	get_position()
+	#get_position()
 	#click(463,172)
 
 	"""for n in range(30):
@@ -98,6 +128,8 @@ if __name__ == '__main__':
 		sleep(10)
 	pyg.alert("FINALIZADO !")
 	"""
-	click_scroll()
+
+	#click_scroll()
+	click_post()
 
 	
